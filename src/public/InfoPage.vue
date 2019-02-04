cf66a222a517ce6be713dd7c2b7b099119fd0817
Artanic <599253487@qq.com>
2019-02-05 00:05:47 +0800

    update student info

--- src/public/InfoPage.vue
+++ src/public/InfoPage.vue
@@ -5,6 +5,16 @@
           <v-na></v-na>
         </el-col>
     </el-row>
+    <el-row :gutter="2">
+      <el-col :span="4" align="middle">
+        <el-menu class="midway">
+          <img v-bind:src="img2" class="Img" v-if="getState">
+          <img v-bind:src="img" class="Img" v-else>
+        </el-menu>
+      </el-col>
+      <el-col :span="18">
+      </el-col>
+    </el-row>
     </div>
 </template>
 
@@ -14,14 +24,26 @@ import na from './Navigation'
 export default {
   data () {
     return {
+      img: require('../assets/student.jpg'),
+      img2: require('../assets/instructor.jpg')
     }
   },
   components: {
     'v-na': na
+  },
+  computed: {
+    getState () {
+      return this.$store.state.instructor
+    }
   }
 }
 </script>
 
 <style scoped>
-
+.midway {
+  min-height: 1000px;
+}
+  .Img {
+    height: 150px;
+  }
 </style>
